33897a2b697953c9baadb191bbb7f1dca8fb1349
Stefan Rotaru <stefanrotaru@hotmail.com>
2024-06-08 18:03:16 +0300

    add heating logic

---
+++ subsystems/heating.py
@@ -0,0 +1,19 @@
+from core.parts import Component, Rule, Sensor
+import random
+
+class Radiator(Component):
+    pass
+
+
+class TemperatureRule(Rule):
+    def apply(self, reading, radiator):
+        if reading < 18:
+            radiator.switch_on()
+        elif rading > 22:
+            radiator.switch_off()
+
+
+class TemperatureSensor(Sensor):
+    def take_reading(self):
+        # simulate a temperature reading
+        return random.randint(16, 25)
